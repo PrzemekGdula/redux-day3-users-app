0e32040e9347755599fb7827e4884b5bfc46582f
PrzemekGdula <przemo.g12@wp.pl>
2019-04-17 10:08:38 +0200

    Setting users in redux works

--- src/Users.js
+++ src/Users.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
 import { connect } from 'react-redux'
+import {setUsersActionCreator} from './state/users'
 
-const Users = (props) => (
+const Users = (props) => {
+    props._setUsers('ala')
+
+    return(
     <div>
         Users
     </div>
 )
+}
 
 const mapStateToProps = state => ({})
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    _setUsers: (users)=> dispatch(setUsersActionCreator(users)),
+})
 
 export default connect(
-
     mapStateToProps,
     mapDispatchToProps
 )(Users)
