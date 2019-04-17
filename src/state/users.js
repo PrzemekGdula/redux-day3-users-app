6923fe8634eafc3417adab7b0722a52e7e908006
PrzemekGdula <przemo.g12@wp.pl>
2019-04-17 11:34:44 +0200

    dispatching async action when store is created

--- src/state/users.js
+++ src/state/users.js
@@ -2,35 +2,35 @@ const SET = 'users/SET'
 
 export const fetchUsersAsyncActionCreator = () => (dispatch, getState) => {
 
-    fetch('https://randomuser.me/api')
-        .then(r => r.json())
-        .then(data => {
-            dispatch(
-                setUsersActionCreator(
-                    data.results
-                )
-            )
-        })
+  fetch('https://randomuser.me/api')
+    .then(r => r.json())
+    .then(data => {
+      dispatch(
+        setUsersActionCreator(
+          data.results
+        )
+      )
+    })
 
 }
 
 export const setUsersActionCreator = users => ({
-    type: SET,
-    users,
+  type: SET,
+  users,
 })
 
 const initialState = {
-    users: null,
+  users: null,
 }
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case SET:
-            return {
-                ...state,
-                users: action.users,
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case SET:
+      return {
+        ...state,
+        users: action.users,
+      }
+    default:
+      return state
+  }
 }
